180ad518aeae0c65b4fad1976ad401445f5491f4
Tim Butler <tbutler1132@gmail.com>
2025-12-13 11:41:58 -0500

    feat: define initial types

---
+++ src/core/types.ts
@@ -0,0 +1,32 @@
+export interface EntityType {
+  id: string;
+  name: string;
+  description: string;
+  fields: Field[];
+}
+
+export interface Field {
+  name: string;
+  description: string;
+  type: "string" | "number" | "enum";
+}
+
+export interface Entity {
+  id: string;
+}
+
+export interface Synth extends Entity {
+  name: string;
+  horizon: Horizon;
+  preferredObservation: string;
+  parentSynth: Synth;
+  childrenSynths: Synth[];
+  expectedEffort: "low" | "medium" | "high";
+  priorPreferenceStrength: "low" | "medium" | "high";
+}
+
+export interface Horizon extends Entity {
+  name: string;
+  description: string;
+  level: number;
+}
